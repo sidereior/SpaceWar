b6887a884b45309941b405f4b47a1878d5b7506a
Alex N <84296624+sidereior@users.noreply.github.com>
2021-11-21 21:23:35 -0500

    fixed super command

--- SpaceTreasure.java
+++ SpaceTreasure.java
@@ -1,8 +1,14 @@
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.imageio.*;
+
 public class SpaceTreasure extends Sprite {
 
 
-    public SpaceTreasure(double left, double top, int width, int height, String image, String location) {
-        super(left, top, width, height, image, location);
+    public SpaceTreasure(double left, double top, int width, int height, String image, String location, Color[] colors) {
+        super(left, top, width, height, image, location, colors);
     }
 
 
